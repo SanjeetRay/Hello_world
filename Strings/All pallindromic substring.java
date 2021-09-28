7a28faade439be953b5bf8086ad99544d2614ea4
Sanjeet ray <87923556+SanjeetRay@users.noreply.github.com>
2021-09-28 12:41:21 -0700

    Create All pallindromic substring.java

---
+++ Strings/All pallindromic substring.java
@@ -0,0 +1,34 @@
+import java.util.*;
+class Test2{
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        String str = scn.nextLine();
+        for(int i=0;i<str.length();i++)
+        {
+            for(int j=i+1;j<=str.length();j++)
+            {
+                String s = str.substring(i,j);
+                if(isPallindrome(s)==true)
+                   System.out.println(s);
+            }
+        }
+        
+    }
+    public static boolean isPallindrome(String str)
+    {
+        int l =0;
+        int r = str.length()-1;
+        boolean isPallindrome = true;
+        while(l<r)
+        {
+            if(str.charAt(l)!=str.charAt(r))
+              {
+                  isPallindrome = false;
+                  break;
+              }
+              l++;
+              r--;
+        }
+        return isPallindrome;
+    }
+}
