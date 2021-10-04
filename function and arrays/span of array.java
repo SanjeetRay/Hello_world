693cba24fc91a905dcc50acaa093b2ec4a0edc3a
Sanjeet ray <87923556+SanjeetRay@users.noreply.github.com>
2021-10-04 10:29:39 -0700

    Create span of array.java

---
+++ function and arrays/span of array.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+
+public static void main(String[] args) throws Exception {
+   Scanner scn = new Scanner(System.in);
+   int n = scn.nextInt();
+   int[] arr = new int[n];
+   for(int i=0; i<n ;i++)
+    arr[i] = scn.nextInt();
+    
+    int max = Integer.MIN_VALUE;
+    int min = Integer.MAX_VALUE;
+    for(int i=1; i<n ;i++)
+    {
+        if(arr[i]<min){
+            min = arr[i];
+        }
+        if(arr[i]>max)
+        {
+            max = arr[i];
+        }
+    }
+    System.out.println(max-min);
+ }
+
+}
