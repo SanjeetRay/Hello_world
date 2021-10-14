815f1982e6e1b0b33782189402fdba7dd579f479
Sanjeet ray <87923556+SanjeetRay@users.noreply.github.com>
2021-10-14 15:12:26 +0530

    Create Searching sorted 2D array.java

---
+++ 2d arrays/Searching sorted 2D array.java
@@ -0,0 +1,50 @@
+/*
+
+
+package com.company;
+import java.io.*;
+import java.util.*;
+
+public class Test3 {
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        Scanner scn = new Scanner(System.in);
+        int r1 =  scn.nextInt();
+
+
+        int[][] arr = new int[r1][r1];
+
+        for (int i = 0; i < r1; i++)
+        {
+            for (int j = 0; j < r1; j++)
+            {
+                arr[i][j] = scn.nextInt();
+            }
+        }
+        int ele = scn.nextInt();
+
+        int i= 0, j=arr[0].length-1;
+        int count =0;
+        while(j>=0)
+        {
+            if(ele>arr[i][j])
+            {
+                i++;
+
+            }
+            else if(ele<arr[i][j])
+            {
+                j--;
+            }
+            else if(arr[i][j]==ele)
+            {
+                System.out.println("element found at "+i+ ","+ j);
+                return;
+            }
+        }
+        System.out.println("element not found");
+
+    }
+
+}
