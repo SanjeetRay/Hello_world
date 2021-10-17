80ada0e2a54619c522178db261ceff3225c301d0
Sanjeet ray <87923556+SanjeetRay@users.noreply.github.com>
2021-10-17 14:54:29 +0530

    Create Difference of every two consecutive characters.java

---
+++ Strings/Difference of every two consecutive characters.java
@@ -0,0 +1,64 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static String solution(String str){
+	    String res ="";
+	    char ch1='a';
+	    char ch2='b';
+		for(int i=0;i<str.length()-1;i++)
+		{
+		    ch1 = str.charAt(i);
+		    
+		      ch2  = str.charAt(i+1);
+		    res+=(ch1)+Integer.toString((ch2-ch1));
+		    
+		    /*
+		    
+		    res+=ch1;
+		    res += ch2-ch1;
+		    
+		    */
+		}
+         
+		return res+ch2;
+	}
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		String str = scn.next();
+		System.out.println(solution(str));
+	}
+  
+   //--------------------------------------------------------------------------------------------------------
+    /* Using StringBuilder
+    public static String solution(String str){ 
+      StringBuilder res = new StringBuilder("");
+	    char ch1='a';
+	    char ch2='b';
+		 for(int i=0;i<str.length()-1;i++)
+	  	{
+		    ch1 = str.charAt(i);
+		    
+		    ch2  = str.charAt(i+1);
+		    res.append((ch1)+Integer.toString((ch2-ch1)));
+		    
+		    
+		   // res+=ch1;
+		   // res += ch2-ch1;
+		    
+		    
+	  	}
+         
+	  	return res.append(ch2);
+	  }
+  	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+	    StringBuilder str = new StringBuilder(scn.nextLine());	
+		System.out.println(solution(str));
+    
+    }
+    
+    */
+
+}
