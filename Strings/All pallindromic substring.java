59af2766586b38e2966fbf4985bc9c216f169f72
Sanjeet ray <87923556+SanjeetRay@users.noreply.github.com>
2021-10-17 14:46:24 +0530

    Update All pallindromic substring.java

--- Strings/All pallindromic substring.java
+++ Strings/All pallindromic substring.java
@@ -10,37 +10,48 @@ c
 
 */
 
+import java.io.*;
 import java.util.*;
-class Test2{
-    public static void main(String[] args){
-        Scanner scn = new Scanner(System.in);
-        String str = scn.nextLine();
-        for(int i=0;i<str.length();i++)
-        {
-            for(int j=i+1;j<=str.length();j++)
-            {
-                String s = str.substring(i,j);
-                if(isPallindrome(s)==true)
-                   System.out.println(s);
-            }
-        }
-        
-    }
-    public static boolean isPallindrome(String str)
+
+public class Main {
+    
+    public static boolean isPalindrome(String str)
     {
-        int l =0;
-        int r = str.length()-1;
-        boolean isPallindrome = true;
-        while(l<r)
+        int left = 0;
+        int right = str.length()-1;
+        while(left<right)
         {
-            if(str.charAt(l)!=str.charAt(r))
-              {
-                  isPallindrome = false;
-                  break;
-              }
-              l++;
-              r--;
+            if(str.charAt(left)!=str.charAt(right))
+              return false;
+             left++;
+             right--;
         }
-        return isPallindrome;
+        return true;
     }
+
+	public static void solution(String str){
+		for(int i=0; i<str.length(); i++)
+		{
+		    for(int j=i+1; j<=str.length(); j++)
+		    {
+		        String sub = str.substring(i,j);
+		        if(isPalindrome(sub)==true)
+		          System.out.println(sub);
+		    }
+		}
+		
+	}
+
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		String str = scn.next();
+        /* 
+        
+        String s = scn.next();
+ 		StringBuilder str = new StringBuilder(s);  // with StringBuilder
+		solution(str.toString());
+        */
+		solution(str);
+	}
+
 }
