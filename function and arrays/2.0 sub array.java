1f0ebc60fbe076afa4690f608bc2ef1a6b26e0a8
Sanjeet ray <87923556+SanjeetRay@users.noreply.github.com>
2021-10-18 16:42:22 +0530

    Update 2.0 sub array.java

--- function and arrays/2.0 sub array.java
+++ function and arrays/2.0 sub array.java
@@ -1,4 +1,32 @@
 /*
+> subsequence in strings = sub set in array. 
+  > it is non contiguous
+  > total no of subsequence/sub set (2^n)
+> whereas substring in string = subarray in arrays. 
+  > it is contiguous
+  > total no of substring/sub array (n(n+1)/2)
+  for a array of size 3 with elements :  10 20 30
+  subarray would be(6) : 
+  10
+  10 20
+  10 20 30  <--- all contguous no two element have gap in between them
+  20
+  20 30 
+  30 
+  
+substring of abc --> [ a, ab, abc, b, bc, c ]
+subsequence -->
+[
+   ---
+   -b-
+   --c
+   -bc
+   a--
+   ab-
+   a-c  <--- non contiguous two elements can have gap between them
+   abc
+ ]
+
 
 arr[3] = 10|20|30|40
 
