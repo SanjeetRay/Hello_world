14a3005efd13570d75369064306f0a9f34c2b9cf
Sanjeet ray <87923556+SanjeetRay@users.noreply.github.com>
2021-11-01 02:14:08 +0530

    Update 5.1 print encoding.java

--- Recursion arraylist/5.1 print encoding.java
+++ Recursion arraylist/5.1 print encoding.java
@@ -49,3 +49,38 @@ public class Main {
   }
 
 }
+
+
+// leetcode 91. Decode ways optimized solution | we have to only get count of strings not the string.
+
+class Solution {
+    public int numDecodings(String s) {
+        StringBuilder sb = new StringBuilder(s);
+        int[] dp = new int[sb.length()+1];
+        Arrays.fill(dp, -1);
+        int n = numDecodingRec(sb, 0, new StringBuilder(""), dp);
+        return n;     
+    }
+    public int numDecodingRec(StringBuilder sb, int idx, StringBuilder ans, int[] dp)
+    {
+        if(idx==sb.length())
+            return dp[idx] = 1;
+        char ch1 = sb.charAt(idx);
+        int chval1 = ch1 -'0';
+        if(chval1==0)
+          return 0;
+        if(dp[idx]!=-1)
+            return dp[idx];
+        int count =0; 
+        char code = (char)('a'+ (chval1-1));
+        count += numDecodingRec(sb, idx+1, ans.append(code), dp);
+        if(idx<sb.length()-1){           // checking if the 2 length string is available to call further or not.
+            int chval2 = (sb.charAt(idx)-'0')*10 + (sb.charAt(idx+1) -'0') ;
+            if(chval2<=26){
+                char code2 = (char)('a'+ (chval2-1));
+                count += numDecodingRec(sb, idx+2, ans.append(code2), dp);
+            }
+        }
+        return dp[idx] = count;
+    }
+}
