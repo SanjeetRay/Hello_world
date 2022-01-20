de223ad15d448d16e43c27d071a5f888bf83c318
Sanjeet ray <87923556+SanjeetRay@users.noreply.github.com>
2022-01-21 00:51:13 +0530

    Update 1.1_Bubble sort.java

--- Sorting/1.1_Bubble sort.java
+++ Sorting/1.1_Bubble sort.java
@@ -1,11 +1,11 @@
 // T(n) = On^2
-
+// bubbling the biggest element to the right
 public class Student{  
 
 	 public static void main(String args[]){  
-		int[] arr = {9,1,5,6,2,3};            // bubble sort
-		for(int j=arr.length; j>=2; j--)
-		for(int i=1; i<j;i++)
+		int[] arr = {9,1,5,6,2,3};            
+		for(int j=1; j<=arr.length-1; j++)
+		for(int i=0; i<arr.length-j; i++)
 		{
 			if(arr[i-1]>arr[i])
 			{
